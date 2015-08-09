4be6a7ab488aaa03d85639eac0051c01a0ca998d
Brennan Novak <hi@brennannovak.com>
2015-08-09 23:34:50 +0200

    first commit, whoo hoo :P

---
+++ server.js
@@ -0,0 +1,91 @@
+var fs = require('fs');
+var Hapi = require('hapi');
+var HapiJsonView = require('hapi-json-view');
+var Good = require('good');
+var Path = require('path');
+var _ = require('underscore');
+var Query = require('datapackage-query');
+
+// Create Server
+var server = new Hapi.Server();
+server.connection({ port: 8888 });
+
+server.views({
+  engines: {
+    json: {
+      module: HapiJsonView.create(),
+      compileMode: 'async',
+      contentType: 'application/json'
+    }
+  }
+})
+
+// API Config
+var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+console.log('Loaded API   config for: ' + config.name);
+
+// JSON Api
+server.route({
+  method: 'GET',
+  path: '/{dataset*}',
+  handler: function(request, reply) {
+
+    // Validate is allowed
+    if (_.indexOf(config.datasets, request.params.dataset) > -1) {
+
+      var package_path = config.paths.data + config.prefix + request.params.dataset;
+      var data_path = Path.dirname(package_path + '/datapackage.json');
+
+      console.log('Open datapackage.json in: ' + package_path);
+
+      Query.Grow(package_path + '/datapackage.json', function(schema) {
+      
+        // If Schema Contains Multiple
+        _.each(schema.resources, function(resource, key) {
+      
+          // Open CSV Data
+           Query.Twirl(data_path, resource, function(csv_to_json_data) {
+      
+            var response = _.omit(schema, 'resources');
+            response['status'] = 'success';
+            response['result'] = csv_to_json_data;
+
+            reply(response);
+
+           });
+      
+        });
+
+      });
+    } else {
+      // Show Error
+      reply({
+        status: 'error',
+        name: 'Oops no datasets called [' + request.params.dataset + ']'
+      });
+    }
+
+  }
+});
+
+
+// Good Logging
+server.register({
+  register: Good,
+  options: {
+    reporters: [{
+        reporter: require('good-console'),
+        args:[{ log: '*', response: '*' }]
+    }]
+  }
+}, function (err) {
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  // Start
+  server.start(function () {
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
+});
+
