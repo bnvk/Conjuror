0112946a36ec6f0a267ab6c5eda4933c588aff28
Brennan Novak <hi@bnvk.me>
2016-01-09 14:37:57 +0100

    removed redundant schema path in Conjuror.Grow() and semicolons

--- conjuror.js
+++ conjuror.js
@@ -1,21 +1,21 @@
-var argv          = require('./lib/conjuror.options.js');
-var config        = require('./lib/conjuror.config.js');
+var argv          = require('./lib/conjuror.options.js')
+var config        = require('./lib/conjuror.config.js')
 
 // Run the imported options.
-var args = argv.run();
+var args = argv.run()
 
-var Conjuror = require('./lib/conjuror.basic.js');
+var Conjuror = require('./lib/conjuror.basic.js')
 
 // Load Conjuror Modules
 Conjuror.recipes = require('./lib/conjuror.recipes.js')
-Conjuror.Date = require('./lib/conjuror.date.js');
-Conjuror.Trim = require('./lib/conjuror.trim.js');
-Conjuror.Search = require('./lib/conjuror.search.js');
+Conjuror.Date = require('./lib/conjuror.date.js')
+Conjuror.Trim = require('./lib/conjuror.trim.js')
+Conjuror.Search = require('./lib/conjuror.search.js')
 
 var app_path = __filename.replace('conjuror.js', '')
 
 if (Conjuror.recipes[args.options.recipe] !== undefined) {
-  console.log("We're going to", Conjuror.recipes[args.options.recipe].description);
+  console.log("We're going to", Conjuror.recipes[args.options.recipe].description)
 } else {
   // Else Default and Start It Up
   if (args.options.input !== undefined) {
@@ -23,14 +23,14 @@ if (Conjuror.recipes[args.options.recipe] !== undefined) {
     Conjuror.getIngredients(config.get_file_path(), function(config) {
       // don't really care of the status of config for the moment.
       // let's just supply sensible defaults.
-      args.config = config;
-      args.app_path = app_path;
-      Conjuror.Grow(args.options.input, args);
+      args.config = config
+      args.app_path = app_path
+      Conjuror.Grow(args)
 
     })
   } else {
-    console.log('404 No spell found \nAre you sure you specified an --input -i value');
+    console.log('404 No spell found \nAre you sure you specified an --input -i value')
   }
 }
 
-module.exports = Conjuror;
+module.exports = Conjuror
